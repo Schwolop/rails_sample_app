f19ae06bf41545a883b9da64dd1c870b269ca62d
Tom Allen <tom@jugglethis.net>
2012-01-31 21:16:07 +1100

    Got to section 8.2, got distracted, wrote user index list instead...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,23 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new # Create an empty user, to be defined by submitting the form.
     @title = "Sign up"
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      # Handle a successful save.
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
+  
+  def index
+    @users = User.all
+    render 'index'
   end
 
 end
