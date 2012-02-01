217dfc3258e794b1c9d113d708a6bebb7172dd9e
Tom Allen <tom@jugglethis.net>
2012-02-01 23:15:06 +1100

    Done with sign in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper # Provide the sessions helper to all controllers 
+    # (as well as in views where helpers are available by default).
 end
